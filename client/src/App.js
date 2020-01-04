1ff44ad5fe1e696f8bd46d547e5da054ea2e5886
Zheyang Cen <cenzheyang@gmail.com>
2020-01-04 00:15:29 -0800

    small updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './component/NavigBar';
-import SubmitForm from './component/SubmitForm';
 import ViewBox from './component/ViewBox'
 
 function App() {
