4144930a6a940de3c43db4ff5746309add8244ca
Zheyang Cen <cenzheyang@gmail.com>
2020-01-04 00:14:19 -0800

    add a submit result indicator under the sumit form

---
+++ client/src/component/SubmitResult.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {
+    Alert
+} from 'react-bootstrap'
+
+function SubmitResult(props) {
+    return (
+        <Alert variant='success'>
+            Successful
+        </Alert>    
+    );
+}
+
+export default SubmitResult;
--- client/src/component/ViewBox.js
+++ client/src/component/ViewBox.js
@@ -3,12 +3,14 @@ import {
     Card, 
 } from 'react-bootstrap'
 import SubmitForm from './SubmitForm'
+import SubmitResult from './SubmitResult'
 
 function ViewBox(props) {
     return (
         <Card style={{ margin: 'auto'}}>
             <Card.Body>
                 <SubmitForm></SubmitForm>
+                <SubmitResult></SubmitResult>
             </Card.Body>
         </Card>
     );
