e010cf97bc95b298787bdd00988fed58d6c3c90d
Zheyang <zheyangc@zillowgroup.com>
2020-02-12 17:16:23 -0800

    add shell script

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin.bash
+
+echo "update apt" 
+sudo apt update 
+
+echo "install curl" 
+sudo apt install curl
+
+echo "install nodejs and npm"
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+echo "install nginx"
+sudo apt install nginx
+
+echo "install mongodb"
+sudo apt install -y mongodb
+
+echo "install nodemon"
+sudo npm install nodemon -g
+
+echo "install pm2"
+sudo npm install pm2 -g
