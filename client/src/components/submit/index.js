416ae5358cf00c4b62bb74f46b7a890ae32a23c3
Zheyang <zheyangc@zillowgroup.com>
2020-04-10 12:07:30 -0700

    update card format

--- client/src/components/submit/index.js
+++ client/src/components/submit/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { SubmitForm } from "./SubmitForm";
 import { SubmitResult } from "./SubmitResult";
+import { Card } from 'react-bootstrap';
 
 export const Submit = () => (
-  <React.Fragment>
-    <SubmitForm />
-    <SubmitResult />
-  </React.Fragment>
+  <Card className="mx-auto my-5 w-75" style={{ margin: 'auto'}}>
+    <Card.Body>
+      <SubmitForm />
+      <SubmitResult />
+    </Card.Body>
+  </Card>
 );
