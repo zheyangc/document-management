42100d543f8cd78e5d05c6cbe511bc1631c52ae8
Zheyang <zheyangc@zillowgroup.com>
2020-04-10 17:39:14 -0700

    remove style

--- client/src/components/submit/index.js
+++ client/src/components/submit/index.js
@@ -4,10 +4,8 @@ import { SubmitResult } from "./SubmitResult";
 import { Card } from 'react-bootstrap';
 
 export const Submit = () => (
-  <Card className="mx-auto my-5 w-75" style={{ margin: 'auto'}}>
-    <Card.Body>
-      <SubmitForm />
-      <SubmitResult />
-    </Card.Body>
-  </Card>
+  <React.Fragment>
+    <SubmitForm />
+    <SubmitResult />
+  </React.Fragment>
 );
