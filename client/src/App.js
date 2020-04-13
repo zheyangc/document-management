93c5f5e0bc49ccad5a6ea0b375a52b5924d77c99
Zheyang Cen <cenzheyang@gmail.com>
2020-04-12 19:58:20 -0700

    add some notes for login

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavigationBar from "./components/NavigBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Submit } from "./components/submit";
 import { Search } from "./components/search";
+import { Login } from "./components/login";
 import { DocumentContextProvider } from "./contexts/DocumentContext";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Submit} />
           <Route path="/submit" component={Submit} />
           <Route path="/search" component={Search} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Router>
     </DocumentContextProvider>
---
+++ client/src/components/login/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Login = () => (
+  <p>登录功能正在开发法中...</p>
+);
