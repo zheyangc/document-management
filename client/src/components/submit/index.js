d7f4bb534cb26b5a0fc5e2bb3e26835e4ffff392
Zheyang Cen <cenzheyang@gmail.com>
2020-04-12 18:02:23 -0700

    update some style

--- client/src/components/submit/index.js
+++ client/src/components/submit/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { SubmitForm } from "./SubmitForm";
 import { SubmitResult } from "./SubmitResult";
+import { Card } from "react-bootstrap";
 
 export const Submit = () => (
-  <React.Fragment>
-    <SubmitForm />
-    <SubmitResult />
-  </React.Fragment>
+  <Card>
+    <Card.Body>
+      <SubmitForm />
+      <SubmitResult />
+    </Card.Body>
+  </Card>
 );
