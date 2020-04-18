56334cf59d633352de6a452ff4d888ba32b18f5a
Zheyang Cen <cenzheyang@gmail.com>
2020-04-17 18:12:20 -0700

    update authreducer

--- client/src/reducers/AuthReducer.js
+++ client/src/reducers/AuthReducer.js
@@ -1,3 +1,7 @@
+export const LOGIN_SUCCEEDED = "LOGIN_SUCCEEDED";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+export const RESET = "RESET";
+
 export const initialState = {
   isAuthenticated: false,
   userInfo: null,
@@ -7,21 +11,21 @@ export const initialState = {
 export const authReducer = (state, action) => {
   console.log(action);
   switch (action.type) {
-    case "LOGIN_SUCCEEDED": {
+    case LOGIN_SUCCEEDED: {
       return {
         ...state,
         isAuthenticated: true,
         userInfo: action.payload,
       };
     }
-    case "LOGIN_FAILED": {
+    case LOGIN_FAILED: {
       return {
         ...state,
         isAuthenticated: false,
         error: action.payload,
       };
     }
-    case "RESET": {
+    case RESET: {
       return initialState;
     }
     default:
